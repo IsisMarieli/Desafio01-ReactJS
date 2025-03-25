a929373e32e213e62bafb258f4795cd5468b7201
IsisMarieli <Isismarieli1@gmail.com>
2025-03-25 17:00:43 -0300

    first commit

---
+++ toDo-List/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Header } from './components/Header'
+
+import './Global.css';
+
+export function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
